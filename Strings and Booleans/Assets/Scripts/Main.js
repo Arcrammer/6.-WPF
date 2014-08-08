835033958850ec0b4f111c96cf16b6a542b3741b
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-08 00:06:55 -0400

    Studying apostrophes and quotation marks
    - Added a phrase using surrounding quotation marks enclosing an
    apostrophe
    - Added another phrase using an escape sequence to escape the
    apostrophe in the word "don't"

--- Strings and Booleans/Assets/Scripts/Main.js
+++ Strings and Booleans/Assets/Scripts/Main.js
@@ -1,2 +1,4 @@
 var isStudent = true; // Boolean
 var firstName = "Alexander"; // String
+var phrase = "I don't know."; // Surrounding quotation marks enclosing an apostrophe
+var anotherPhrase = 'I don\'t know.' // Uses an escape sequence to escape the apostrophe in the word "don't"
