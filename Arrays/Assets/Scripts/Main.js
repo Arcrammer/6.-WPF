9c3985d937745dad91045c5ad38743d5d9694647
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-08 00:27:18 -0400

    Using a variable to find the index of the array
    - Now using a variable containing the array index value ’15’ to call
    the 15th object of the ‘muppetNames’ array
    - Fixed a type where I’d forgotten the closing parenthesis of
    console.log()

--- Arrays/Assets/Scripts/Main.js
+++ Arrays/Assets/Scripts/Main.js
@@ -3,5 +3,7 @@ var muppetNames = ["Kermit","Miss Piggy","Gonzo","Rizzo"];
 muppetNames[1] = "Rizzo";
 console.log(muppetNames[1]);
 
+var num = 15;
+
 muppetNames[15] = "Fozzy";
-console.log(muppetNames[15];
+console.log(muppetNames[15]);
