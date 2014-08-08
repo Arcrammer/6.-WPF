00013226d848000c8247c52729ddbcb4714e72e1
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-08 00:11:50 -0400

    More text added, using new line escape sequence
    - Added more text to the variable ‘anotherPhrase’
    - Used a new line escape sequence in the variable ‘anotherPhrase’

--- Strings and Booleans/Assets/Scripts/Main.js
+++ Strings and Booleans/Assets/Scripts/Main.js
@@ -1,4 +1,4 @@
 var isStudent = true; // Boolean
 var firstName = "Alexander"; // String
 var phrase = "I don't know."; // Surrounding quotation marks enclosing an apostrophe
-var anotherPhrase = 'I don\'t know.' // Uses an escape sequence to escape the apostrophe in the word "don't"
+var anotherPhrase = 'I don\'t know.\n You\'ll need to speak with Professor Garlic.' // Uses an escape sequence to escape the apostrophes in the words "don't" and 'You'll', also begins a new line using '\n'
