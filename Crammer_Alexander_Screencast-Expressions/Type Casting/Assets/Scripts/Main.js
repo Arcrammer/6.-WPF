882bf3fe7c866064bdeac648f7435fba5a8eb7b5
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:51:30 -0400

    Added number format

--- Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
@@ -2,6 +2,6 @@ var area = 212;
 var first = 555;
 var last = 5555;
 
-var phoneNumber = String(area) + String(first) + String(last);
+var phoneNumber = "(" + String(area) + ") " + String(first) + " - " + String(last);
 
 document.write(phoneNumber);
