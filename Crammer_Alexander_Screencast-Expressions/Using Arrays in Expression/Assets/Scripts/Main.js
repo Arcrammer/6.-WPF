dbd1a67e1a6b433c20e0ed1e39db57a7113ea534
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:12:14 -0400

    Added necessary assets

---
+++ Crammer_Alexander_Screencast-Expressions/Using Arrays in Expression/Assets/Scripts/Main.js
@@ -0,0 +1,8 @@
+var quizOne = 87;
+var quizTwo = 100;
+var quizThree = 60;
+var quizFour = 80;
+
+// Average is found by dividing the sum of the items with the amount of items
+
+var average = (quizOne + quizTwo + quizThree + quizFour) / 4; // Division last
