8bb09ba27d706a2358d5fea37acc298a4e8e205c
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:42:43 -0400

    Type juggle from string to integer

--- Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
@@ -1,4 +1,6 @@
  var integerWithinString = "6";
- var compose = 7 + integerWithinString; // Composes 'integerWithinString' value as a string with the string '7'
+ var compose = 7 + Number(integerWithinString); // Composes 'integerWithinString' value as an integer with the string '7' after converting it to an integer
+ 
+ // What is the difference between Number() and toInt()?
  
  document.write(compose);
