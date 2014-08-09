6cfc34917ac24a7343ebf0eb812ed0bc15d35564
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 16:48:32 -0400

    'Slice of Pie part 2' added

--- Expressions_worksheet/js/Main.js
+++ Expressions_worksheet/js/Main.js
@@ -14,10 +14,15 @@ document.write("<br /><br />"); // Two line breaks, just to keep everything tidy
 // SLICE OF PIE PART 1
 /* A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza each partygoer will get at the party.  (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.) */
 
-var slicesEach = 8; // Amount of slices cut through the pizza
-var partyGuestAmount = 32; // Amount of guests at the party
 var pizzas = 8; // Amount of pizzas ordered
+var slicesEach = 8; // Amount of slices cut through the pizza
+var partyGuestAmount = 31; // Amount of guests at the party
 var slicesPerGuest = pizzas * slicesEach / partyGuestAmount; // Calculates the amount each guest could have if the pizza were split among each of the guests by multiplying 'pizzas' and 'slicesEach' to find the amount of slices there are from all of the pizzas ordered then dividing the amount returned with the amount of guests at the party, stored in 'partyGuestAmount'
 document.write("Each person ate " + slicesPerGuest + " slices of pizza at the party.");
 document.write("<br /><br />"); // Two more line breaks, just to keep everything tidy!
+
+// SLICE OF PIE PART 2
+/* At the pizza party Sparky, the host’s dog is excited, because he gets the leftover pizza after the slices have been divided up evenly among the guests. Assume guests get whole slices, how many whole slices will Sparky feast on? */
+
+document.write("Sparky got " pizzas * slicesEach % slicesPerGuest + " slices of pizza."); // I have no idea if this is right. I've sersiouly sat here and though about how in the world I'd find the answer throughout the entire evening. Anyway, I've found the remainder of the amount of slices each guest would be given by multiplying the amount of pizza with the amount of slices in each pizza, then using the modulous operator to find the remainder of the division between the total amount of pizza slices and party guests. I apologise in advance if this is incorrect, but I'm going with it because I have no idea what else it could be!
 
