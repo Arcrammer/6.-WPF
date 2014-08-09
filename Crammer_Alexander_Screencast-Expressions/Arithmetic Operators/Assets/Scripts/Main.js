b97816854f64579e392eea13df11d6fc5f5e8cc1
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 13:51:44 -0400

    Added calculation logic and writing to the document
    - Added calculation to find the area of a triangle given its’ width and
    height
    - Prints the result to the document

--- Crammer_Alexander_Screencast-Expressions/Arithmetic Operators/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Arithmetic Operators/Assets/Scripts/Main.js
@@ -2,5 +2,6 @@
 
 var width = 4;
 var height = 5;
-var area = width * height;
+var area = width * height / 2; // Multiply the width and height of the triangle then divide the result with 2 in order to find the area of the triangle
 
+document.write(area); // Print the result of 'area' to the document
