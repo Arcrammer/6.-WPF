0eb98f89f178ecbace012491ba5958fbac60124a
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 13:21:20 -0400

    Added another variable to add with the first
    - Added ‘b’ variable
    - Added missing ‘var’ before ‘a’ variable

--- Crammer_Alexander_Screencast-Expressions/Expressions/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Expressions/Assets/Scripts/Main.js
@@ -1,3 +1,4 @@
-a = 2;
+var a = 2;
+var b = a + 3;
 
 document.write(2); // I know it's bad to use document.write() because it throws out everything, but it shouldn't matter in this case. I'll use it for all of these assignments.
