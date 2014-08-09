ba3def4fc772f5e9ca5188bbe5ac1bc9af930ffd
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 13:18:31 -0400

    Clean-up
    - Moved folders from ‘HTML and JavaScript Setup’ to one folder in root
    - Created folder ‘Crammer_Alexander_Screencast-Expressions’ for
    relative assignment
    - Created assets for first screencast of the ‘Screencast: Expressions’
    assignment and second

---
+++ Crammer_Alexander_Screencast-Expressions/Expressions/Assets/Scripts/Main.js
@@ -0,0 +1,3 @@
+a = 2;
+
+document.write(2); // I know it's bad to use document.write() because it throws out everything, but it shouldn't matter in this case. I'll use it for all of these assignments.
