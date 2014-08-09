ae5d3562cc7ed332a22fc525c68c860bce5e59ae
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:06:15 -0400

    Created calculation to find the value of the quiz score

--- Crammer_Alexander_Screencast-Expressions/Order of Operations/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Order of Operations/Assets/Scripts/Main.js
@@ -1,5 +1,8 @@
 var quizOne = 87;
 var quizTwo = 100;
 var quizThree = 60;
-var quizForu = 80;
+var quizFour = 80;
 
+// Average is found by dividing the sum of the items with the amount of items
+
+var average = quizOne + quizTwo + quizThree + quizFour;
