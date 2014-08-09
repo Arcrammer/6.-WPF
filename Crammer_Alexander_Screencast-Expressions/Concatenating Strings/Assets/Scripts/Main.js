ddfd491d238f7358a8b0d082c7d9cc847de96173
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:22:09 -0400

    Added space
    - Added space between names using concatenation

--- Crammer_Alexander_Screencast-Expressions/Concatenating Strings/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Concatenating Strings/Assets/Scripts/Main.js
@@ -1,5 +1,5 @@
 var firstName = "Kermit";
 var lastName = "the Frog"; // I don't know if I'd call that a name xD
-var fullName = firstName + lastName;
+var fullName = firstName + " " + lastName;
 
 document.write(fullName);
