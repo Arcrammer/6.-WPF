4f5ef13482ce3ec7cad8f85df22214e72721563d
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:46:39 -0400

    Phone number - Adding integers
    - Removed previous type juggling code
    - Added new variables to hold the values of three parts of a phone
    number
    - Adds the values of the phone number integers together then outputs
    the result to the document using document.write()

--- Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
@@ -1,6 +1,7 @@
- var integerWithinString = "6";
- var compose = 7 + Number(integerWithinString); // Composes 'integerWithinString' value as an integer with the string '7' after converting it to an integer
- 
- // What is the difference between Number() and toInt()?
- 
- document.write(compose);
+var area = 212;
+var first = 555;
+var last = 5555;
+
+var phoneNumber = area + first + last;
+
+document.write(phoneNumber);
