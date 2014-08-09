da89f0007fcf8af96809157f23ba84188f91ed9e
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:37:58 -0400

    Added comments

--- Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Type Casting/Assets/Scripts/Main.js
@@ -1,4 +1,4 @@
  var integerWithinString = "6";
- var compose = 7 + integerWithinString;
+ var compose = 7 + integerWithinString; // Composes 'integerWithinString' value as a string with the string '7'
  
  document.write(compose);
