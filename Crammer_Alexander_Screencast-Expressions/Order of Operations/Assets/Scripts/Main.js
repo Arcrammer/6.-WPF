876d4e496fcd7b7b6a11c4f6cea18c3357fa19c2
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 14:08:47 -0400

    Added parenthesis so division will be last

--- Crammer_Alexander_Screencast-Expressions/Order of Operations/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Order of Operations/Assets/Scripts/Main.js
@@ -5,4 +5,4 @@ var quizFour = 80;
 
 // Average is found by dividing the sum of the items with the amount of items
 
-var average = quizOne + quizTwo + quizThree + quizFour / 4; // Division first
+var average = (quizOne + quizTwo + quizThree + quizFour) / 4; // Division last
