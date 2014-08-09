a04b7b24f2174abb4b240bef67a52dc743c77110
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-09 13:40:26 -0400

    Given documents appropriate <title>'s
    - I hadn’t been giving my index’s the proper titles, so I’ve decided to
    go through all of my documents and apply the appropriate titles

--- Crammer_Alexander_Screencast-Expressions/Example Expressions/Assets/Scripts/Main.js
+++ Crammer_Alexander_Screencast-Expressions/Example Expressions/Assets/Scripts/Main.js
@@ -3,4 +3,4 @@ var year = new Date().getFullYear(); // Get the current year and return it as YY
 
 var age = year - yearBorn;
 
-document.write(age);
+document.write(age); // This will print the amount of years from my birth year. Because my birth date is December 27 -- far from January -- the returned value will likely be inaccurate relevant to my birth date
