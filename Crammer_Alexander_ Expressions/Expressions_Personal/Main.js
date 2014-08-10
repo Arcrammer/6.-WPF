d31143417fa2bdd5f136b3b7d413425b4b1e994e
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 19:38:33 -0400

    Created necessary variables
    - Created the Date() object
    - Created variables for the show time formatted between the hour and
    minute of the hour it will air
    - Created variables to fetch the current time in the format of hours
    then minute of the hour
    - Created a .write() to output the result to the document for debugging
    and primarily being sure everything at this point would return what I
    was expecting it to — what I’d had in mind.

--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -2,3 +2,12 @@
 	August 10, 2014
 	Expressions	(Personal)	*/
 
+var date = new Date();
+var hour = date.getHours();
+var minute = date.getMinutes();
+
+var showTimeHour = 14; // The hour the show will air. Valid input is 0 to 24
+var showTimeMinute = 30; // The hours minute in which the show will air. Valid input is 0 to 60
+var currentTime = date.getHours() + ":" + date.getMinutes(); // Returns the current hour and minute in the formats: '19:34' or '5:14'
+
+document.write(currentTime); // Writes the result of 'currentTime' to the document so I know it's working
