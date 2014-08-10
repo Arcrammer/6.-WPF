1c9cd10edd29b170fe00645c31fb3f7ddf6a8ee6
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 19:13:36 -0400

    Forgotton Commit

--- Crammer_Alexander_Prompt-and-Alert/js/Main.js
+++ Crammer_Alexander_Prompt-and-Alert/js/Main.js
@@ -2,4 +2,5 @@ var width = prompt("What is the width of the rectangle?");
 var height = prompt("What is the height of the rectangle?");
 var area = width * height;
 
-document.write(area);
+//document.write(area);
+alert("The area of your rectangle is " + area + ".");
