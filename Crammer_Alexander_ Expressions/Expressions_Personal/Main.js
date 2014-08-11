ca3fc9d05997f014ebf7ac72856e1ad4b90edf09
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 20:20:23 -0400

    More comments

--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -6,5 +6,6 @@
 
 var gallonCapacity = 35; // Capacity of the fuel tank in gallons
 var mpg = 30; // Miles per gallon of the vehicle to be driven
-var distance = gallonCapacity * mpg;
+var distance = gallonCapacity * mpg; // Returns the amount of miles the vehicle can drive without refueling
 
+document.write("Your vehicle will make around " + distance + " miles before it will need more fuel.");
