b019a35cd8372df13407b4c4df4860355fca3004
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 16:06:14 -0400

    New industry calculator idea

--- Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
+++ Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
@@ -3,4 +3,5 @@
 	Assignment: Expressions	*/
 
 // INDUSTRIAL CALCULATOR
-// 
+// This calculator will calculate the amount of email you'd read a day based on the amount of time you plan to spend reading your email, and it will also tell you if you'll have enough time to read all of the emails you plan to within a certain span of time.
+
