ca492688a5375ec46245a4f70bc2d0e4f7c2c625
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-11 18:50:00 -0400

    Added prompt()'s for Wacky Calculator

--- Crammer_Alexander_Expressions/Expressions_Wacky/js/main.js
+++ Crammer_Alexander_Expressions/Expressions_Wacky/js/main.js
@@ -5,6 +5,6 @@
 // WACKY CALCULATOR
 // Calculates how many ducks can drive each truck in  a fleet of trucks
 
-var trucks = ; // Amount of trucks in the fleet
-var ducks = ; // Amount of ducks which can drive the trucks
-var trans = ; // Determine the transmission of the trucks, manual or automatic
+var trucks = prompt("How many trucks will be driven by the duckies?"); // Amount of trucks in the fleet
+var ducks = prompt("How many duckies do you have to drive the trucks?"); // Amount of ducks which can drive the trucks
+var trans = prompt("Which transmission type do your trucks use? (Enter 'manual' or 'automatic')"); // Determine the transmission of the trucks, manual or automatic
