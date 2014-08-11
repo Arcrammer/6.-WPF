7581fcb9748202e9dd9620b86be20e8cd51607d0
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-11 17:39:34 -0400

    Completed Industrial Calculator

--- Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
+++ Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
@@ -3,13 +3,18 @@
 	Assignment: Expressions	*/
 
 // INDUSTRIAL CALCULATOR
-// This calculator will calculate the amount of email you'd read a day based on the amount of time you plan to spend reading your email, and it will also tell you if you'll have enough time to read all of the emails you plan to within a certain span of time.
+// This calculator will calculate the amount of email you'll need to read each day based on the amount of time you plan to spend reading your email, and it will also tell you if you'll have enough time to read all of the emails you plan to within a certain span of time.
 
 var emails = prompt("How many emails would you like to read in total?"); // Amount of emails you plan to read
 var readingPerDay = prompt("How many emails do you plan to read each day?"); // Amount of emails you plan to read each day
 var time = prompt("How many days would you like to spend reading these emails?"); // Amount of days you plan to spend reading the emails each day
 
-var toReadDaily = emails / readingPerDay; // Returns the amount of emails to be read daily
-var daysToRead = 24 / toReadDaily;
+var daysToRead = emails / readingPerDay; // Returns the amount of emails to be read daily
 
-console.log("You'll need to read " + toReadDaily + " emails each day, so it will take " + daysToRead + " to read each of the email messages.");
+if(daysToRead > time) {
+	willReadOrNot = "will not";
+} else {
+	willReadOrNot = "will";
+}
+
+console.log("It will take " + daysToRead + " days to read each of the emails, so you " + willReadOrNot + " have the time to read all of your emails within the time you'd like to.");
