7a0a8af46aa4228dade15a48bb03a805f1115eb1
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 14:45:45 -0400

    Created download ETA calculator

--- Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
@@ -2,3 +2,10 @@
 	August 10, 2014
 	Expressions	(Industrial)	*/
 
+var connection = 30; // Mbps speed of your connection
+var downloadSize = 500; // Mb of the file you're downloading
+var totalMb = connection * downloadSize; // Multiplys the connection speed and download size
+var seconds = totalMb / 60; // Returns the seconds of the download time
+var minutes = Math.round(seconds / 60); // Minutes of the download, rounded
+
+document.write(minutes); // Writes the eta in minutes to the document
