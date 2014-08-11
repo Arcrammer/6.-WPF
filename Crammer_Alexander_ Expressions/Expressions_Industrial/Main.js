2c9d84481309c1cfad2e2ef518dfe5ea4c646cf2
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 14:13:10 -0400

    Scrapped delivery idea
    I’d realised industrial will be relative to my industry.

--- Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
@@ -2,8 +2,3 @@
 	August 10, 2014
 	Expressions	(Industrial)	*/
 
-var packages = 750; // Total amount of packages to be shipped by fleet
-var truckPackageCapacity = 50; // Amount of packages each truck can or will carry to deliver
-var trucksNeeded = packages / truckPackageCapacity ; // Amount of trucks within the fleet needed to deliver the packages
-var days = 4; // Amount of days to deliver all of the packages
-
