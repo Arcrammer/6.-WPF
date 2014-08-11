ec5599f1d559c16e4ffad1c6fab12ca6eb5714fd
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 14:12:24 -0400

    Began Industrial Calculator

--- Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
@@ -2,7 +2,8 @@
 	August 10, 2014
 	Expressions	(Industrial)	*/
 
-// Just coming up with an idea
-var packages = 175; // Total amount of packages to be shipped by fleet
-var trucks = 20; // Amount of trucks within the fleet to deliver the packages
-var truckPackageCapacity = ; // Amount of packages each truck can or will carry to deliver
+var packages = 750; // Total amount of packages to be shipped by fleet
+var truckPackageCapacity = 50; // Amount of packages each truck can or will carry to deliver
+var trucksNeeded = packages / truckPackageCapacity ; // Amount of trucks within the fleet needed to deliver the packages
+var days = 4; // Amount of days to deliver all of the packages
+
