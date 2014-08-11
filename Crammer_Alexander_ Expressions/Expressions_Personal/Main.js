66e0bf14fddbc043e5bb985ba7760909db3cd2ca
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 20:24:48 -0400

    Changed default values
    I’ve changed the default values to be more realistic

--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -4,7 +4,7 @@
 
 // 'How Far Your Car Will go Before it Will Need More Gasoline' calculator
 
-var gallonCapacity = 35; // Capacity of the fuel tank in gallons
+var gallonCapacity = 25; // Capacity of the fuel tank in gallons
 var mpg = 30; // Miles per gallon of the vehicle to be driven
 var distance = gallonCapacity * mpg; // Returns the amount of miles the vehicle can drive without refueling
 
