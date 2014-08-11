07ad1a24159f3a40e875c430b8ae2197d9b49ec9
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 20:12:10 -0400

    Became frustrated because of math, restarting

--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -2,9 +2,3 @@
 	August 10, 2014
 	Expressions	(Personal)	*/
 
-var date = new Date(); // Creates the Date() object
-var hour = date.getHours(); // Returns the current hour in the format '0' to '24'
-var showTimeHour = 14; // The hour the show will air. Valid input is '0' to '24'
-var something = showTimeHour * hour;
-
-document.write(something);
