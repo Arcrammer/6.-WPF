74016ead0359005292cd45c22f5acb5fb2686196
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 20:17:08 -0400

    New calculator idea, created necessary variables
    - Decided to create a calculator to determine how far your vehicle will
    go before needing a refuel
    - Created variables for the calculator

--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -2,3 +2,9 @@
 	August 10, 2014
 	Expressions	(Personal)	*/
 
+// 'How Far Your Car Will go Before it Will Need More Gasoline' calculator
+
+var gallonCapacity = 35; // Capacity of the fuel tank in gallons
+var mpg = 30; // Miles per gallon of the vehicle to be driven
+var distance = gallonCapacity * mpg;
+
