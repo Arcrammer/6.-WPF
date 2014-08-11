342fa61ac4b6b922f7ade1967b6bd0c7582f51a1
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-10 20:08:36 -0400

    Debugging, deciding how the calculator will work
    - Struggling with the calculator

--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -2,12 +2,9 @@
 	August 10, 2014
 	Expressions	(Personal)	*/
 
-var date = new Date();
-var hour = date.getHours();
-var minute = date.getMinutes();
+var date = new Date(); // Creates the Date() object
+var hour = date.getHours(); // Returns the current hour in the format '0' to '24'
+var showTimeHour = 14; // The hour the show will air. Valid input is '0' to '24'
+var something = showTimeHour * hour;
 
-var showTimeHour = 14; // The hour the show will air. Valid input is 0 to 24
-var showTimeMinute = 30; // The hours minute in which the show will air. Valid input is 0 to 60
-var currentTime = date.getHours() + ":" + date.getMinutes(); // Returns the current hour and minute in the formats: '19:34' or '5:14'
-
-document.write(currentTime); // Writes the result of 'currentTime' to the document so I know it's working
+document.write(something);
