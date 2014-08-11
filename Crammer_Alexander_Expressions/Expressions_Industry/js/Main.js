0841487101f9d4f9a373d33a4360ff07a8557a49
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 16:08:44 -0400

    Added prompt()'s

--- Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
+++ Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
@@ -5,7 +5,7 @@
 // INDUSTRIAL CALCULATOR
 // This calculator will calculate the amount of email you'd read a day based on the amount of time you plan to spend reading your email, and it will also tell you if you'll have enough time to read all of the emails you plan to within a certain span of time.
 
-var emails = ; // Amount of emails you plan to read
-var readingPerDay = ; // Amount of emails you plan to read each day
-var time = ; // Amount of hours you plan to spend reading the emails each day
+var emails = prompt("How many emails would you like to read in total?"); // Amount of emails you plan to read
+var readingPerDay = prompt("How many emails do you plan to read each day?"); // Amount of emails you plan to read each day
+var time = prompt("How much time are you going to spend reading these emails on a daily basis?"); // Amount of hours you plan to spend reading the emails each day
 
