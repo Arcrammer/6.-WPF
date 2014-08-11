cdd1ce0d56e50a39f99e0da4a4345eefd8c8b70a
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 15:29:27 -0400

    Created Personal Calculator
    This calculator will display the amount of gasoline needed to travel a
    particular distance. It will also display the amount of refuels the car
    will need based on its’ fuel capacity in gallons
    
    - Created necessary files and basic structure
    - Created necessary variables

---
+++ Crammer_Alexander_Expressions/Expressions_Personal/js/main.js
@@ -0,0 +1,10 @@
+/*	Alexander Rhett Crammer
+	Monday, August 11, 2014
+	Assignment: Expressions	*/
+
+// PERSONAL CALCULATOR
+// This calculator will find the amount of gasoline you'll need to travel a particular amount of miles
+
+var miles = prompt("How many miles will you be travelling?"); // Amount of miles to travel
+var mpg = prompt("What is your vehicles' fuel economy in miles per gallon?"); // Miles per gallon of your vehicle
+var capacity = prompt("What is your tank capacity in gallons?"); // Amount of gallons your vehicle can carry
