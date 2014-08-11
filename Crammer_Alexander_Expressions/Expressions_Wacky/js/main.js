8a93574755fdc4bf728c3ed2a21db3a1f6ee4c17
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-11 18:48:31 -0400

    Created primary variables

--- Crammer_Alexander_Expressions/Expressions_Wacky/js/main.js
+++ Crammer_Alexander_Expressions/Expressions_Wacky/js/main.js
@@ -3,5 +3,8 @@
 	Assignment: Expressions	*/
 
 // WACKY CALCULATOR
-//
+// Calculates how many ducks can drive each truck in  a fleet of trucks
 
+var trucks = ; // Amount of trucks in the fleet
+var ducks = ; // Amount of ducks which can drive the trucks
+var trans = ; // Determine the transmission of the trucks, manual or automatic
