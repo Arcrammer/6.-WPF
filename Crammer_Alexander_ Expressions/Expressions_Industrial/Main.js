7e50df5304b4bdc9905ca0dfccc29105e40c336f
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 04:13:32 -0400

    Just coming up with an idea here
    - Created some memos in ‘Main.js’ of ‘Expressions_Personal’
    - Found an idea for ‘Expressions_Industrial’ then created variables and
    comments to demonstrate it
    
    Good night!

--- Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Industrial/Main.js
@@ -2,3 +2,7 @@
 	August 10, 2014
 	Expressions	(Industrial)	*/
 
+// Just coming up with an idea
+var packages = 175; // Total amount of packages to be shipped by fleet
+var trucks = 20; // Amount of trucks within the fleet to deliver the packages
+var truckPackageCapacity = ; // Amount of packages each truck can or will carry to deliver
--- Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
+++ Crammer_Alexander_ Expressions/Expressions_Personal/Main.js
@@ -9,3 +9,6 @@ var mpg = 30; // Miles per gallon of the vehicle to be driven
 var distance = gallonCapacity * mpg; // Returns the amount of miles the vehicle can drive without refueling
 
 document.write("Your vehicle will make around " + distance + " miles before it will need more fuel.");
+
+// NEED THREE GIVENS, HAVE TWO
+// CREATE 'js' FOLDERS FOR SCRIPTS
