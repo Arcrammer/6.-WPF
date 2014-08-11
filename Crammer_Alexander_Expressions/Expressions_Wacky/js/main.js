a0812842162ec3ab1164a9d1b3514d7afba04a0a
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-11 19:12:32 -0400

    Added algorithm

--- Crammer_Alexander_Expressions/Expressions_Wacky/js/main.js
+++ Crammer_Alexander_Expressions/Expressions_Wacky/js/main.js
@@ -8,3 +8,11 @@
 var trucks = prompt("How many trucks will be driven by the duckies?"); // Amount of trucks in the fleet
 var ducks = prompt("How many duckies do you have to drive the trucks?"); // Amount of ducks which can drive the trucks
 var trans = prompt("Which transmission type do your trucks use? (Enter 'manual' or 'automatic')"); // Determine the transmission of the trucks, manual or automatic
+
+var ducksForTheTrucks = ducks / trucks; // Returns the amount of ducks for each truck
+
+if(toLowerCase(trans) == 'manual') {
+	var ducksTrans = ducksForTheTrucks + 2; // Amount of ducks to drive if manual (one duck more for the stick and another duck for the clutch)
+}
+
+document.write("With one duckie at each pedal, two at the wheel, one at the stick, and another at the clutch, you'll need " + ducksTrans + " duckies to drive your truckies.");
