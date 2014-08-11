a405e01938fb8892c2bf91d4c44a7998d045a681
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 16:03:51 -0400

    Scrapped 'website time' calculator
    I wouldn’t have enough givens to use that idea

--- Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
+++ Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
@@ -3,6 +3,4 @@
 	Assignment: Expressions	*/
 
 // INDUSTRIAL CALCULATOR
-// This calculator will calculate the amount of time you'll have to create a website based on the amount of pages it will have
-
-
+// 
