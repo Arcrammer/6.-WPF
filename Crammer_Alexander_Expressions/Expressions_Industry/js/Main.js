4a9bee962bc613a8bba6e552edf0a2ef74c765e5
Arcrammer <Alexander@Alexanders-MacBook-Pro.local>
2014-08-11 15:31:46 -0400

    Created assets for industrial calculator

---
+++ Crammer_Alexander_Expressions/Expressions_Industry/js/Main.js
@@ -0,0 +1,7 @@
+/*	Alexander Rhett Crammer
+	Monday, August 11, 2014
+	Assignment: Expressions	*/
+
+// INDUSTRIAL CALCULATOR
+// This calculator will find the amount of gasoline you'll need to travel a particular amount of miles
+
