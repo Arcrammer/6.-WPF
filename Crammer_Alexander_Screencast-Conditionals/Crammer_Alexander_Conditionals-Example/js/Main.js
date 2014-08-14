9c45b6f69d9ce1aae083937b4a136b0a4903de92
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 14:48:59 -0400

    Completed 'Conditionals Example'

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Conditionals-Example/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Conditionals-Example/js/Main.js
@@ -3,10 +3,11 @@
 	Thursday, 14 August, 2014.
 	Screencast: Conditionals	*/
 
-var oldEnough = true;
+var oldEnough = false;
 
 // If the child is old enough we'll print to the console "You can ride."
 if(oldEnough) {
 	// If the condition is true
 	console.log("You can ride.");
 };
+console.log("-- this comes after --");
