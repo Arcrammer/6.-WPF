dd38413d201369d104196a62f9a7554c0884e740
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 15:43:24 -0400

    Creating variables

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Logical-Operators/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Logical-Operators/js/Main.js
@@ -3,3 +3,12 @@
 	Thursday, 14 August, 2014.
 	Screencast: Conditionals	*/
 
+var budget = 300;
+var iPhonePrice = 199.99;
+var pay = 200;
+
+if(iPhonePrice > budget) {
+	console.log("You can buy an iPhone!");
+} else {
+	console.log("No iPhone for you :(");
+}
