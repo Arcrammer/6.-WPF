8f839067b2b4a437ad53c6d66fe02a4e0d64eab8
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 17:56:39 -0400

    Created variable to hold the converted value from celsius to fahrenheit
    - Created a variable which uses the algebraic formula to calculate the
    fahrenheit value of the entered celsius temperature

--- Crammer_Alexander_Conditionals-Worksheet/js/Main.js
+++ Crammer_Alexander_Conditionals-Worksheet/js/Main.js
@@ -8,6 +8,7 @@
 var degrees = prompt("Which tempurature would you like to convert?");
 var unit = prompt("Please enter 'F' to convert the previously entered tempurature to celsius, or 'C' to convert it to fahrenheit.");
 var inCelsius = (degrees - 32) * 5 / 9; // The expression of this variable uses the algebraic formula to convert fahrenheit to celsius
+var inFahrenheit = degrees * 1.8 + 32; // The expression of this variable uses the algebraic formula to convert celsius to fahrenheit
 
 if(unit === "F") {
 	// Convert to fahrenheit from celsius
