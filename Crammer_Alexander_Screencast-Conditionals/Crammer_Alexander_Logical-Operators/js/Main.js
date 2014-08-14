bb6a53eda6b28d32a0f23e6b07e07ca138f0a2cf
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 15:51:39 -0400

    Added || operator

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Logical-Operators/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Logical-Operators/js/Main.js
@@ -3,11 +3,11 @@
 	Thursday, 14 August, 2014.
 	Screencast: Conditionals	*/
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var pay = 200;
+var wonLottery = true;
 
-if(iPhonePrice < budget && pay > 300) /* If the iPhone is less than budget  && if pay is more than 300 */ {
+if(iPhonePrice < budget || wonLottery === true) /* If the iPhone is less than budget  && if pay is more than 300 */ {
 	console.log("You can buy an iPhone!");
 } else /* Otherwise */ {
 	console.log("No iPhone for you :(");
