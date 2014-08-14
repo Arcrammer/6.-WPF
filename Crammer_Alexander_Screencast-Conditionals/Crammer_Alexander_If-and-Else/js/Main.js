01c78b5facc0cc23556fd2ae209bc9a45fac295f
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 15:20:40 -0400

    Added opposite

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_If-and-Else/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_If-and-Else/js/Main.js
@@ -3,3 +3,14 @@
 	Thursday, 14 August, 2014.
 	Screencast: Conditionals	*/
 
+var kidHeight = 47;
+var minHeight = 48;
+
+// If the child is old enough we'll print to the console "You can ride."
+if(kidHeight + sneakerLift > minHeight) /* If the kid is over 48 inches of height */ {
+	// If the condition is true
+	console.log("You can ride.");
+};
+if(kidHeight < minHeight) {
+	console.log("Sorry, kid! You have some growing to do.");
+}
