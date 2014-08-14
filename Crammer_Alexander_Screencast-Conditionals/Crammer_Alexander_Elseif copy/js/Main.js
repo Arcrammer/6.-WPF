ea362ca5d086d889b735f39daf039fde15de868f
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 15:23:59 -0400

    Created assets for 'Else if'

---
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Elseif copy/js/Main.js
@@ -0,0 +1,16 @@
+/*	Alexander Rhett Crammer
+	WPF 1408 Section 1
+	Thursday, 14 August, 2014.
+	Screencast: Conditionals	*/
+
+var kidHeight = 47;
+var minHeight = 48;
+
+// If the child is old enough we'll print to the console "You can ride."
+if(kidHeight > minHeight) /* If the kid is over 48 inches of height */ {
+	// If the condition is true
+	console.log("You can ride.");
+} else {
+	// Otherwise
+	console.log("Sorry, kid! You have some growing to do.");
+}
