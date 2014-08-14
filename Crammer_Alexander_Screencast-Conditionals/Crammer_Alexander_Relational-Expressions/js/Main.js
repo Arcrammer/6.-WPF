ab4f886e5723e67241e289d6155744c43ecfb484
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 14:52:30 -0400

    Set 'kidHeight' to 40

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Relational-Expressions/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Relational-Expressions/js/Main.js
@@ -3,3 +3,11 @@
 	Thursday, 14 August, 2014.
 	Screencast: Conditionals	*/
 
+var kidHeight = 30;
+
+// If the child is old enough we'll print to the console "You can ride."
+if(kidHeight > 40) /* If the kid is over 48 inches of height */ {
+	// If the condition is true
+	console.log("You can ride.");
+};
+console.log("-- this comes after --");
