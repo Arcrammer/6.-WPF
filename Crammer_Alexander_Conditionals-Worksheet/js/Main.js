64c45c2d6d12394de33966e671f6dd92f67ce5b7
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 17:49:09 -0400

    Outlined if()...else if()...else statement
    - Created an if()...else if()...else statement to either calculate the
    conversion from either the unit fahrenheit to celsius or celsius to
    fahrenheit, or inform the user they’ve entered a string other than
    either ‘F’ or ‘C’ in the ‘unit’ prompt.

--- Crammer_Alexander_Conditionals-Worksheet/js/Main.js
+++ Crammer_Alexander_Conditionals-Worksheet/js/Main.js
@@ -8,3 +8,13 @@
 var degrees = prompt("Which tempurature would you like to convert?");
 var unit = prompt("Please enter 'F' to convert the previously entered tempurature to celsius, or 'C' to convert it to fahrenheit.");
 
+if(unit === "F") {
+	// Convert to fahrenheit from celsius
+	
+} else if(unit === "C") {
+	// Convert to celsius from fahrenheit
+	
+} else {
+	// Tell the user they've entered anything other than 'F' or 'C' in 'unit'
+	
+}
