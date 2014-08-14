639ffc2527905725b80f3b017272cfa2eebee0d8
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 16:04:41 -0400

    Added ternary value to 'book'

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Ternary-Operators/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Ternary-Operators/js/Main.js
@@ -3,14 +3,14 @@
 	Thursday, 14 August, 2014.
 	Screencast: Conditionals	*/
 
-var age = 11;
-var book;
+var age = 6;
+var book = (age > 10) ? "Green Eggs and Ham" : "The Time Machine";
 
 // If the child is under 10 they'll be given "Green Eggs and Ham" otherwise "The Time Machine"
-if(age > 10) {
+/* if(age > 10) {
 	var book = "Green Eggs and Ham";
 } else {
 	var book = "The Time Machine";
-}
+} */
 
 console.log(book);
