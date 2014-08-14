926b20e17f8ac435964bed38eb9bf8dd2f9d7346
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-14 15:58:22 -0400

    Created assets for 'Ternary Operators'

--- Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Logical-Operators/js/Main.js
+++ Crammer_Alexander_Screencast-Conditionals/Crammer_Alexander_Logical-Operators/js/Main.js
@@ -7,7 +7,7 @@ var budget = 100;
 var iPhonePrice = 199.99;
 var wonLottery = true;
 
-if(iPhonePrice < budget || wonLottery === true) /* If the iPhone is less than budget  && if pay is more than 300 */ {
+if(iPhonePrice < budget || wonLottery) /* If the iPhone is less than budget  && if pay is more than 300 */ {
 	console.log("You can buy an iPhone!");
 } else /* Otherwise */ {
 	console.log("No iPhone for you :(");
