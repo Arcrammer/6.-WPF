aac61d16a3f0c262d7496b5009132de6d271c810
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-15 17:46:36 -0400

    Debugging and creating a boolean from user input
    - Added forgotten link to ‘Main.js’ in each ‘index.html’
    - Created an if()…else if() to juggle the user input for
    ‘weekendDrinker’ from string to boolean
    - Using .toLowerCase() to prevent errors if the user inputs capital
    letters in ‘weekendDrinker’

--- Crammer_Alexander_Conditionals/Conditionals_Personal/js/Main.js
+++ Crammer_Alexander_Conditionals/Conditionals_Personal/js/Main.js
@@ -5,8 +5,15 @@
 
 // Calculates how long a bag of coffee will last for daily coffee drinkers
 
-var packSize = prompt(""); // Determines the size of the bag of coffee in pounds
-var brewingDaily = prompt(""); // Determines the amount of coffee being brewed each day in tablespoons
-var weekendDrinker = prompt(""); // Determines if the user drinks coffee during the weekends
+var packSize = prompt("How many pounds are in your bag of coffee?"); // Determines the size of the bag of coffee in pounds
+var brewingDaily = prompt("How many tablespoons are you brewing on a daily basis?"); // Determines the amount of coffee being brewed each day in tablespoons
+var weekendDrinker = prompt("Do you brew on the weekends? ('yes' or 'no')"); // Determines if the user drinks coffee during the weekends
+var weekendDrinker = weekendDrinker.toLowerCase();
+
+if(weekendDrinker === "yes" || weekendDrinker === "true") {
+	weekendDrinker = true ; // Set if the user does drink coffee on the weekends
+} else if (weekendDrinker === "no" || weekendDrinker === "false") {
+	weekendDrinker = false; // Set if the user doesn't drink coffee on the weekends
+}
 
 
