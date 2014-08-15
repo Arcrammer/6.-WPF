0d60199779c32d9fb4b106751d3fa4f1bb4c377f
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-15 17:30:39 -0400

    Created given variables
    - Created ‘packSize’, ‘brewingDaily’, and ‘weekendDrinker’

--- Crammer_Alexander_Conditionals/Conditionals_Personal/js/Main.js
+++ Crammer_Alexander_Conditionals/Conditionals_Personal/js/Main.js
@@ -4,3 +4,9 @@
 	Conditionals	*/
 
 // Calculates how long a bag of coffee will last for daily coffee drinkers
+
+var packSize = prompt(""); // Determines the size of the bag of coffee in pounds
+var brewingDaily = prompt(""); // Determines the amount of coffee being brewed each day in tablespoons
+var weekendDrinker = prompt(""); // Determines if the user drinks coffee during the weekends
+
+
