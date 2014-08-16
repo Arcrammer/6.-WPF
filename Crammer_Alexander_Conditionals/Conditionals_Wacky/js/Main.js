44959f6785f943fcae75587f46b06c407ef72501
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-15 21:19:03 -0400

    Added empty string handling
    - Now the user will be prompted again if an empty string is provided
    and a message will be shown using console.log()

--- Crammer_Alexander_Conditionals/Conditionals_Wacky/js/Main.js
+++ Crammer_Alexander_Conditionals/Conditionals_Wacky/js/Main.js
@@ -7,6 +7,17 @@
 // Calculates the amount of water needed to fill a pool
 
 var gallonsToFill = prompt("How many gallons of water will you fill the pool with?"); // Amount of water to fill the pool in gallons
+
+if(gallonsToFill === "") {
+	console.log("Please enter the amount of water you'll be filling the pool with."); // Alert the user to provide an amount of water to fill the pool with
+	var gallonsToFill = prompt("How many gallons of water will you fill the pool with?"); // Prompts the user again in the case they've provided an empty string
+}
+
 var gpm = prompt("How fast will the water fill the pool in gallons per second?"); // Rate of water filling the pool in gallons per miniute
+
+if(gpm === "") {
+	console.log("Please enter the speed of the water source which will fill the pool."); // Alert the user to provide a speed of the water source filling the pool
+	var gpm = prompt("How fast will the water fill the pool in gallons per second?"); // Prompts the user again in the case they've provided an empty string
+}
 
 
