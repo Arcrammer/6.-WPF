144611ed30d1792be9c1cfbb20c9d249062a17b8
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-16 16:15:30 -0400

    Added comments and 'average' variable
    - Added some comments
    - Created ‘average’ to hold the average amount of commits each day

--- Crammer_Alexander_Conditionals/Conditionals_Industry/js/Main.js
+++ Crammer_Alexander_Conditionals/Conditionals_Industry/js/Main.js
@@ -9,11 +9,11 @@ var commits = prompt("How many commits have been made?"); // Asks the user for t
 var days = prompt("How many days have passed between the first and last commits?"); // Asks the user for the amount of days between the first commit and last commit
 
 if(commits === "") {
-	console.log("You've forgotten to enter the amount of commits.");
+	console.log("You've forgotten to enter the amount of commits."); // Alerts the user they've forgotten to provide input for 'commits'
 	var commits = prompt("How many commits have been made?"); // Asks the user for the total amount of commits to account for
 } else if(days === "") {
-	console.log("You've forgotten to enter the amount of days between the commits.");
+	console.log("You've forgotten to enter the amount of days between the commits."); // Alerts the user they've forgotten to provide input for 'days'
 	var days = prompt("How many days have passed between the first and last commits?"); // Asks the user for the amount of days between the first commit and last commit
 }
 
-
+var average = commits / days; // Returns the average amount of commits each day
