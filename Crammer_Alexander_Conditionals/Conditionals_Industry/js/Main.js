56e4486d52b8dd9933acc546b647492e57372719
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-16 16:02:19 -0400

    Created necessary variables

--- Crammer_Alexander_Conditionals/Conditionals_Industry/js/Main.js
+++ Crammer_Alexander_Conditionals/Conditionals_Industry/js/Main.js
@@ -5,4 +5,7 @@
 
 // Calculates the average amount of commits to a Git repository
 
+var commits = prompt(); // Asks the user for the total amount of commits to account for
+var days = prompt(); // Asks the user for the amount of days between the first commit and last commit
+
 
