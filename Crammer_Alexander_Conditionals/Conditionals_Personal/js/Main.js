96f16b2ff46a2c3ffd85302fd4e11539e11371fd
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-15 20:48:42 -0400

    Created logic to provide result
    - Created variable ‘willLast’ to hold the amount of instances (days in
    this case) the user will be able to drink coffee supplied from the bag
    of coffee
    - Added an else to warn the user of input in ‘weekendDrinker’ other
    than the strings ‘yes’ or ‘no’ then prompt() them again for the inputs
    either ‘yes’ or ‘no’

--- Crammer_Alexander_Conditionals/Conditionals_Personal/js/Main.js
+++ Crammer_Alexander_Conditionals/Conditionals_Personal/js/Main.js
@@ -14,6 +14,9 @@ if(weekendDrinker === "yes" || weekendDrinker === "true") {
 	weekendDrinker = true ; // Set if the user does drink coffee on the weekends
 } else if (weekendDrinker === "no" || weekendDrinker === "false") {
 	weekendDrinker = false; // Set if the user doesn't drink coffee on the weekends
+} else {
+	console.log("Please enter either 'yes' or 'no' to determine if you drink on the weekends");
+	var weekendDrinker = prompt("Do you brew on the weekends? ('yes' or 'no')"); // Determines if the user drinks coffee during the weekends
 }
 
 // There are 0.5 ounces in each US tablespoon
@@ -22,7 +25,8 @@ if(weekendDrinker === "yes" || weekendDrinker === "true") {
 
 if(weekendDrinker === false) {
 	// Calculation if the user does not brew coffee during the weekends
+	var willLast = 	packSize / (brewingDaily / 2); // Returns the amount of instances remaining in which the user will have enough coffee to brew another cup
 } else if(weekendDrinker === true) {
 	// Calculation if the user does brew coffee during the weekends
-	
+	willLast = (packSize / (brewingDaily / 2)) / 5; // Returns the amount of weeks the user will have enough coffee in the pack to brew another cup
 }
