f61d1e23ac262835db59d10464b9d79f78506276
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-27 18:59:34 -0400

    Calling the function
    - Added copied text

--- Crammer_Alexander_Function-Invocation/js/Main.js
+++ Crammer_Alexander_Function-Invocation/js/Main.js
@@ -2,3 +2,13 @@
 	Wednesday, 27 August, 2014
 	Web Programmng Fundamentals	*/
 
+function outputMsg() {
+	console.log("Hello, world.");
+}
+
+function calculateArea() {
+	width = 20;
+	height = 30;
+	area = width * height;
+	console.log(area);
+}
