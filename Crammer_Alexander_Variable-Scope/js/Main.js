d90dc5d337aabb256a88cbcf84928734c3eb2433
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-27 19:02:55 -0400

    Created asset for 'Variable Scope'

---
+++ Crammer_Alexander_Variable-Scope/js/Main.js
@@ -0,0 +1,12 @@
+/*	Alexander Rhett Crammer
+	Wednesday, 27 August, 2014
+	Web Programmng Fundamentals	*/
+
+function calculateArea() {
+	width = 20;
+	height = 30;
+	area = width * height;
+	console.log(area);
+}
+
+calculateArea();
