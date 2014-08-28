65e5d8ad74cde03aeef1c6bd8dd0ed98891e4de7
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 18:52:05 -0400

    Created variables needed for 'Wacky'

--- Crammer_Alexander_Functions/Wacky/js/Main.js
+++ Crammer_Alexander_Functions/Wacky/js/Main.js
@@ -2,5 +2,10 @@
 	Thursday, 28 August, 2014
 	Functions (Wacky)	*/
 
-// Calculates how much sugar you consume in a month
+// Calculates how much sugar you consume in a given time span
+
+var dailyConsumption = prompt("How many cups of sugar are you consuming on a daily basis?"); // Asks the user for the amount of sugar they're consuming daily
+
+var daySpan = prompt("For how many days would you like to determine your total sugar consumption?"); // Asks the user for the amount of days to use in the calculation
+
 
