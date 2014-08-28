6e09c9f37ae4184bbb7b8075eab4121ae8a0a29e
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 16:27:12 -0400

    Created another while() to check for empty input

--- Crammer_Alexander_Functions/Personal/js/Main.js
+++ Crammer_Alexander_Functions/Personal/js/Main.js
@@ -6,10 +6,15 @@
 
 var amountOfDoggyFood = prompt("How many ounces of food do you have stored for your doggy?"); // Asks the user to input the amount of food they have stored for the doggy to eat
 
-/* Checks for empty input */
+/* Checks for empty input for 'amountOfDoggyFood' */
 while(amountOfDoggyFood === "") {
 	var amountOfDoggyFood = prompt("How many ounces of food do you have stored for your doggy?");
 
 }
 
 var feedingEachDay = prompt("How many ounces are you feeding your doggy on a daily basis?"); // Asks the user to input the amount of food they're feeding their dog each day
+
+/* Checks for empty input for 'feedingEachDay' */
+while(feedingEachDay === "") {
+	var feedingEachDay = prompt("How many ounces are you feeding your doggy on a daily basis?");
+}
