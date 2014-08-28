f747202197e8e9c193780c7a710bcac4da1783cf
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 17:06:03 -0400

    Prompts the user for their upload files size in Mb

--- Crammer_Alexander_Functions/Industry/js/Main.js
+++ Crammer_Alexander_Functions/Industry/js/Main.js
@@ -5,3 +5,4 @@
 // Calculates upload eta based on upload speed and file size
 
 var uploadSpeed = prompt("What is your upload speed in Mbps?"); // Prompts the user for their upload speed in Mbps
+var uploadFileSize = prompt("What is the size of the file you're uploading in Mb?"); // Prompts the user for their upload files' size
