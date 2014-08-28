2cd4a3c5144c3aeb62fa428c4c9c0fd7fa256bac
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 16:22:02 -0400

    Created variable 'feedingEachDay'

--- Crammer_Alexander_Functions/Personal/js/Main.js
+++ Crammer_Alexander_Functions/Personal/js/Main.js
@@ -5,3 +5,4 @@
 // Calculates how long dog food will last based on the amount you have and the amoung you're feeding your doggy each day
 
 var amountOfDoggyFood = prompt("How many ounces of food do you have stored for your doggy?"); // Asks the user to input the amount of food they have stored for the doggy to eat
+var feedingEachDay = prompt("How many ounces are you feeding your doggy on a daily basis?"); // Asks the user to input the amount of food they're feeding their dog each day
