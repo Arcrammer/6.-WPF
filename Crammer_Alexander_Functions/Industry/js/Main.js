6e8e1b28213cd595a2e3544011a7fe26c96bb63c
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 17:28:53 -0400

    Many updates and some debugging
    - New variable ‘usersPatience’ to ask the user if they’re patient
    - Converting ‘usersPatience’ input to lowercase
    - Either converting ‘usersPatience’ input to a boolean value or
    prompting them for input again if they’ve entered anything other than
    ‘yes’ or ‘no’ in the ‘usersPatience’ prompt()
    - Now passing a third parameter to the named function
    ‘howLongWillItTakeToUploadTheFiles’ intended to hold the ‘userPatience’
    value

--- Crammer_Alexander_Functions/Industry/js/Main.js
+++ Crammer_Alexander_Functions/Industry/js/Main.js
@@ -16,8 +16,21 @@ if(uploadFileSize === "" || uploadFileSize === "  " || uploadFileSize === "   ")
 	var uploadFileSize = prompt("Please provide input:\n\nWhat is the size of the file you're uploading in Mb?"); // Prompts the user for input if they've entered a few spaces or an empty string
 }
 
+var usersPatience = prompt("Are you patient? Please enter 'Yes' or 'No'."); // Asks the user to determine their patience on a 'yes' or 'no' level
+
+toLowerCase(usersPatience); // Makes the users' input for 'usersPatience' prompt lowercase regardless of the case of the input provided
+
+/* Either changes the users' input to a boolean value or asks them to provide either 'yes' or 'no' if they've entered anything other than those words, including spaces and meaningless input */
+if(usersPatience != "yes" && usersPatience != "no") /* Carefully using the && logical operator to be sure this statement won't be executed if the user enters either 'yes' or 'no', but instead enters anything other than 'yes' or 'no' */ {
+	var usersPatience = prompt("Please provide input:\n\nAre you patient? Please enter 'Yes' or 'No'."); // Asks the user to determine their patience again
+} else if(usersPatience === "yes") {
+	var usersPatience = true; // Sets 'usersPatience' to a boolean if 'yes' was entered
+} else if(usersPatience === "no") {
+	var usersPatience === false; // Sets 'usersPatience' to a boolean if 'no' was entered
+}
+
 /* Defining the function to provide the result of the calculation */
-function howLongWillItTakeToUploadTheFiles(speed,size) {
+function howLongWillItTakeToUploadTheFiles(speed,size,patience) {
 	var howLongItWillTakeToUpload = size / speed; // Returns the amount of time it will take to upload the files in seconds to the variable 'howLongItWillTakeToUpload'
 	return howLongItWillTakeToUpload;
 }
