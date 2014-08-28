ceff699fde0329669a4a1e26f895aa0f11e08265
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 16:38:42 -0400

    Debugging
    Added missing comparisons for my while() conditionals

--- Crammer_Alexander_Functions/Personal/js/Main.js
+++ Crammer_Alexander_Functions/Personal/js/Main.js
@@ -7,7 +7,7 @@
 var amountOfDoggyFood = prompt("How many ounces of food do you have stored for your doggy?"); // Asks the user to input the amount of food they have stored for the doggy to eat
 
 /* Checks for empty input for 'amountOfDoggyFood' */
-while(amountOfDoggyFood === "" || " " || "  ") {
+while(amountOfDoggyFood === "" || amountOfDoggyFood === " " || amountOfDoggyFood === "  ") {
 	var amountOfDoggyFood = prompt("Please provide input:\n\nHow many ounces of food do you have stored for your doggy?");
 
 }
@@ -15,7 +15,7 @@ while(amountOfDoggyFood === "" || " " || "  ") {
 var feedingEachDay = prompt("How many ounces are you feeding your doggy on a daily basis?"); // Asks the user to input the amount of food they're feeding their dog each day
 
 /* Checks for empty input for 'feedingEachDay' */
-while(feedingEachDay === "" || " " || "  ") {
+while(feedingEachDay === "" || feedingEachDay === " " || feedingEachDay === "  ") {
 	var feedingEachDay = prompt("Please provide input:\n\nHow many ounces are you feeding your doggy on a daily basis?");
 }
 
