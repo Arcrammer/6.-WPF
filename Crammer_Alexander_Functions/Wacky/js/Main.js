869af9b47b26ae5e1fba276805d565e3f56425ee
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 18:53:58 -0400

    Created isNaN conditional for validation of 'dailyConsumption'

--- Crammer_Alexander_Functions/Wacky/js/Main.js
+++ Crammer_Alexander_Functions/Wacky/js/Main.js
@@ -6,6 +6,10 @@
 
 var dailyConsumption = prompt("How many cups of sugar are you consuming on a daily basis?"); // Asks the user for the amount of sugar they're consuming daily
 
+if(isNaN(dailyConsumption)) {
+	var dailyConsumption = prompt("Please enter a numerical value:\n\nHow many cups of sugar are you consuming on a daily basis?"); // Prompts the user to provide numerical input if anything beside a number has been provided
+}
+
 var daySpan = prompt("For how many days would you like to determine your total sugar consumption?"); // Asks the user for the amount of days to use in the calculation
 
 
