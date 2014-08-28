a57a89c86e750cbfa2ed512c7eee0daf9ca441d9
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 16:26:05 -0400

    Created a while() loop to check against empty input

--- Crammer_Alexander_Functions/Personal/js/Main.js
+++ Crammer_Alexander_Functions/Personal/js/Main.js
@@ -5,4 +5,11 @@
 // Calculates how long dog food will last based on the amount you have and the amoung you're feeding your doggy each day
 
 var amountOfDoggyFood = prompt("How many ounces of food do you have stored for your doggy?"); // Asks the user to input the amount of food they have stored for the doggy to eat
+
+/* Checks for empty input */
+while(amountOfDoggyFood === "") {
+	var amountOfDoggyFood = prompt("How many ounces of food do you have stored for your doggy?");
+
+}
+
 var feedingEachDay = prompt("How many ounces are you feeding your doggy on a daily basis?"); // Asks the user to input the amount of food they're feeding their dog each day
