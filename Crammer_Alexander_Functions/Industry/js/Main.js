82a63b6a61b8334e6947935c0e5cf79c7e2f1b72
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 17:12:09 -0400

    Debugging
    - Moved ‘uploadFileSize’ variable below in order to alert the user of
    missing input for ‘uploadSpeed’ before prompting the user to provide
    input for ‘uploadFileSize’

--- Crammer_Alexander_Functions/Industry/js/Main.js
+++ Crammer_Alexander_Functions/Industry/js/Main.js
@@ -5,11 +5,14 @@
 // Calculates upload eta based on upload speed and file size
 
 var uploadSpeed = prompt("What is your upload speed in Mbps?"); // Prompts the user for their upload speed in Mbps
-var uploadFileSize = prompt("What is the size of the file you're uploading in Mb?"); // Prompts the user for their upload files' size
 
 if(uploadSpeed === "" || uploadSpeed === "  " || uploadSpeed === "   ") {
 	prompt("Please provide input:\n\nWhat is your upload speed in Mbps?"); // Prompts the user for input if they've entered a few spaces or an empty string
-} else if(uploadFileSize === "" || uploadFileSize === "  " || uploadFileSize === "   ") {
+}
+
+var uploadFileSize = prompt("What is the size of the file you're uploading in Mb?"); // Prompts the user for their upload files' size
+
+if(uploadFileSize === "" || uploadFileSize === "  " || uploadFileSize === "   ") {
 	var uploadFileSize = prompt("Please provide input:\n\nWhat is the size of the file you're uploading in Mb?"); // Prompts the user for input if they've entered a few spaces or an empty string
 }
 
