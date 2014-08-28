fb297e360587381218f41aa4200e2df40dd88972
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-27 20:01:55 -0400

    Created area calculation function

--- Crammer_Alexander_Arguments-and-Parameters/js/Main.js
+++ Crammer_Alexander_Arguments-and-Parameters/js/Main.js
@@ -2,15 +2,9 @@
 	Wednesday, 27 August, 2014
 	Web Programmng Fundamentals	*/
 
-var width = 5;
-
-function calculateArea() {
-	width = 20;
-	height = 30;
-	area = width * height;
+function calculateArea(w,h) {
+	area = w * h;
 	console.log(area);
 }
 
-calculateArea();
-
-console.log(width);
+calculateArea(30,20);
