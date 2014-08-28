83417a3fb373b0764474e82ce06b8827d2ee4021
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-28 17:38:52 -0400

    More debugging
    - Named function ‘howLongWillItTakeToUploadTheFiles’ will now return
    the amount of time in minutes rather than seconds
    - Named function ‘howLongWillItTakeToUploadTheFiles’ will now return
    output to the user based on their patience
    - (Line 29) Now setting ‘usersPatience’ value to false instead of
    comparing it (haha)
    - (Line 21) Fixed a bug where I was passing ‘usersPatience’ to the
    toLowerCase() method when I should have been appending the method to
    the variable

--- Crammer_Alexander_Functions/Industry/js/Main.js
+++ Crammer_Alexander_Functions/Industry/js/Main.js
@@ -18,7 +18,7 @@ if(uploadFileSize === "" || uploadFileSize === "  " || uploadFileSize === "   ")
 
 var usersPatience = prompt("Are you patient? Please enter 'Yes' or 'No'."); // Asks the user to determine their patience on a 'yes' or 'no' level
 
-toLowerCase(usersPatience); // Makes the users' input for 'usersPatience' prompt lowercase regardless of the case of the input provided
+usersPatience.toLowerCase(); // Makes the users' input for 'usersPatience' prompt lowercase regardless of the case of the input provided
 
 /* Either changes the users' input to a boolean value or asks them to provide either 'yes' or 'no' if they've entered anything other than those words, including spaces and meaningless input */
 if(usersPatience != "yes" && usersPatience != "no") /* Carefully using the && logical operator to be sure this statement won't be executed if the user enters either 'yes' or 'no', but instead enters anything other than 'yes' or 'no' */ {
@@ -26,11 +26,17 @@ if(usersPatience != "yes" && usersPatience != "no") /* Carefully using the && lo
 } else if(usersPatience === "yes") {
 	var usersPatience = true; // Sets 'usersPatience' to a boolean if 'yes' was entered
 } else if(usersPatience === "no") {
-	var usersPatience === false; // Sets 'usersPatience' to a boolean if 'no' was entered
+	var usersPatience = false; // Sets 'usersPatience' to a boolean if 'no' was entered
 }
 
 /* Defining the function to provide the result of the calculation */
 function howLongWillItTakeToUploadTheFiles(speed,size,patience) {
-	var howLongItWillTakeToUpload = size / speed; // Returns the amount of time it will take to upload the files in seconds to the variable 'howLongItWillTakeToUpload'
-	return howLongItWillTakeToUpload;
+	var howLongItWillTakeToUpload = (size / speed) / 60; // Returns the amount of time it will take to upload the files in minutes to the variable 'howLongItWillTakeToUpload'
+	if(patience === true) {
+		var output = console.log("It will take " + howLongItWillTakeToUpload + " minutes to upload the files. Because you're patient, you'll have no problem waiting through this time for the files to upload!");
+	} else if(patience === false) {
+		var output = console.log("It will take " + howLongItWillTakeToUpload + " minutes to upload the files. Because you're nbot patient, you'll have some issues waiting through this time for the files to upload, so I suggest you take a minute to relax and chill out!");
+
+	}
+	return output;
 }
