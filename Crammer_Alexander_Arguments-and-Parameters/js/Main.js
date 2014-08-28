51015e553b3dbf010bced8047e2c1ccc20d37422
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-27 20:03:44 -0400

    Created and defined dogYears() function

--- Crammer_Alexander_Arguments-and-Parameters/js/Main.js
+++ Crammer_Alexander_Arguments-and-Parameters/js/Main.js
@@ -2,9 +2,15 @@
 	Wednesday, 27 August, 2014
 	Web Programmng Fundamentals	*/
 
-function calculateArea(w,h) {
+/*	function calculateArea(w,h) {
 	area = w * h;
 	console.log(area);
 }
 
-calculateArea(30,20);
+calculateArea(30,20);	*/
+
+function dogYears() {
+	var age = 4;
+	var dogYears = age * 7;
+	console.log("Sparky has " + dogYears + " human years.");
+}
