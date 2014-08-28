9a77eda07adf90889726f646b41466c1aa94de87
Arcrammer <Arcrammer@users.noreply.github.com>
2014-08-27 20:00:32 -0400

    Cleaning up and preparing for the video

---
+++ Crammer_Alexander_Arguments-and-Parameters/js/Main.js
@@ -0,0 +1,16 @@
+/*	Alexander Rhett Crammer
+	Wednesday, 27 August, 2014
+	Web Programmng Fundamentals	*/
+
+var width = 5;
+
+function calculateArea() {
+	width = 20;
+	height = 30;
+	area = width * height;
+	console.log(area);
+}
+
+calculateArea();
+
+console.log(width);
